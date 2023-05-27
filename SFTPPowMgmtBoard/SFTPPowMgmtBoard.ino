b232c8d13f37b626f68a52338bce535f97c5f138
Mitchell Baldwin <mitchell@rmbimedical.com>
2023-05-27 16:33:52 -0400

    Added PacketSerial support

--- SFTPPowMgmtBoard/SFTPPowMgmtBoard.ino
+++ SFTPPowMgmtBoard/SFTPPowMgmtBoard.ino
@@ -5,6 +5,7 @@
 */
 
 // the setup function runs once when you press reset or power the board
+#include <PacketSerial.h>
 #include <TaskSchedulerSleepMethods.h>
 #include <TaskSchedulerDeclarations.h>
 #include <TaskScheduler.h>
@@ -25,6 +26,19 @@
 
 constexpr auto BuiltinLEDTogglePeriod = 500;
 
+const uint8_t COMM_BUFFER_SIZE = 0x10;
+const uint8_t PACKET_SIZE = COMM_BUFFER_SIZE - 1;
+
+uint8_t inBuffer[COMM_BUFFER_SIZE];
+uint8_t outBuffer[COMM_BUFFER_SIZE];
+
+uint8_t inPacket[PACKET_SIZE];
+uint8_t outPacket[PACKET_SIZE];
+
+PacketSerial spUSB;							// Serial port driver with COBS encoding
+boolean newCommandMsgReceived = false;		// Flag indicating a new command message was received over the spUSB port
+uint8_t commandMsgReceived = 0x00;			// Initialize the type of command received
+
 // Scheduler
 Scheduler mainScheduler;
 
@@ -32,12 +46,48 @@ void ToggleBuiltinLEDCallback();
 Task taskToggleBuiltinLED(BuiltinLEDTogglePeriod * TASK_MILLISECOND, TASK_FOREVER, &ToggleBuiltinLEDCallback, &mainScheduler, false);
 
 void setup() {
+	
+	// Initialize encoded serial communication
+	spUSB.setPacketHandler(&OnUSBPacket);
+	spUSB.begin(115200);
+
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+
+	spUSB.update();		// Let PacketSerial do its thing
+	mainScheduler.execute();
+
+}
+
+void OnUSBPacket(const uint8_t* buffer, size_t size)
+{
+
+	// Transfer incomming buffer contents (including message type and checksum)	to inPacket
+	for (int i = 0; i < PACKET_SIZE; ++i)
+	{
+		inPacket[i] = buffer[i];
+	}
+
+	// Determine message type
+	commandMsgReceived = buffer[0];
+
+	// Set new message received flag
+	newCommandMsgReceived = true;
+
+}
+
+void SendUSBPacket()
+{
+	int checksum = 0;
+	for (int i = 0; i < PACKET_SIZE - 1; ++i)
+	{
+		checksum += outPacket[i];
+	}
+	outPacket[PACKET_SIZE - 1] = checksum;
+	spUSB.send(outPacket, PACKET_SIZE);
 }
 
 void ToggleBuiltinLEDCallback()
